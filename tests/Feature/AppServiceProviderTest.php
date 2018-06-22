7c557fdfc1611f31eea97086d5b0878bd4b792b0
guitarman <guitarman78@freenet.de>
2018-06-22 13:15:00 +0200

    more tests

--- tests/Feature/AppServiceProviderTest.php
+++ tests/Feature/AppServiceProviderTest.php
@@ -25,30 +25,18 @@ class AppServiceProviderTest extends TestCase
                 ->assertSee(Config::get('app.name'))
         ;
     }
-/*
-    /**
-     * @runInSeparateProcess
-     * @preserveGlobalState disabled
-     * /
+
     public function testCopyright()
     {
-        //Carbon::setTestNow(Carbon::create(2020, 1, 1));
-/*
-        $mock = \Mockery::mock('alias:'.Carbon::class);
-        $mock->shouldReceive('now')
-            ->once()
-            ->andReturn(Carbon::create(2020, 1, 1));
-        $this->app->instance(Carbon::class, $mock);
-* /
-        $user = factory(User::class)->create();
+        Carbon::setTestNow(Carbon::create(2020, 1, 1));
 
-        $response = 
-            $this->actingAs($user)
-                ->from(route('login'))
-                ->get('/')
-                ->assertStatus(200)
-                ->assertSee('Copyright 2018 - 2020')
-        ;
+        $class = new \ReflectionClass('\App\Providers\AppServiceProvider');
+        $method = $class->getMethod('copyright');
+        $method->setAccessible(true);
+        $asp = new \App\Providers\AppServiceProvider($this->app);
+
+        $copyright = $method->invokeArgs($asp, []);
+        $this->assertEquals('Copyright 2018 - 2020', $copyright);
     }
-*/
+
 }
