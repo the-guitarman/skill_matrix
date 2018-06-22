2001801e61d4f3a10c951b7ff98dccb20ad3655f
guitarman <guitarman78@freenet.de>
2018-06-22 10:02:58 +0200

    more model tests

--- tests/Unit/SkillTest.php
+++ tests/Unit/SkillTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Faker\Factory;
-use App\Models\Skill;
+use App\Models\{Skill, UserSkill};
 
 class SkillTest extends TestCase
 {
@@ -19,5 +19,14 @@ class SkillTest extends TestCase
         $skill = factory(Skill::class)->create();
         $this->assertNotEmpty($skill->skillGroup()->get());
         $this->assertEquals(0, $skill->users()->count());
+
+        $userSkill = factory(UserSkill::class)->create([
+            'skill_id' => $skill->id
+        ]);
+        $this->assertEquals(1, $skill->users()->count());
+
+        $user = $skill->users()->first();
+        $this->assertTrue($user->user_skill->grade >= 1);
+        $this->assertTrue($user->user_skill->grade <= 6);
     }
 }
